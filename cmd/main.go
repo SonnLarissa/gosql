69c7ba39a437f611b62e9d1ff17cc0be2fd27b3a
Larisa Kasyanova <lkasyanova@velox.biz>
2021-06-14 10:23:10 +0500

    lesson 28

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"github.com/SonnLarissa/crud/cmd/app"
 	"github.com/SonnLarissa/crud/pkg/customers"
+	"github.com/jackc/pgx/v4/pgxpool"
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"go.uber.org/dig"
 	"log"
 	"net"
-
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -21,140 +23,41 @@ func main() {
 		log.Println(err)
 		os.Exit(1)
 	}
-
 }
 
 func execute(host string, port string, dsn string) (err error) {
-	db, err := sql.Open("pgx", dsn)
+	deps := []interface{}{
+		app.NewServer,
+		http.NewServeMux,
+		func() (*pgxpool.Pool, error) {
+			ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+			return pgxpool.Connect(ctx, dsn)
+		},
+		customers.NewService,
+		func(server *app.Server) *http.Server {
+			return &http.Server{
+				Addr:    net.JoinHostPort(host, port),
+				Handler: server,
+			}
+		},
+	}
+	container := dig.New()
+	for _, dep := range deps {
+		err = container.Provide(dep)
+		if err != nil {
+			return err
+		}
+	}
+	err = container.Invoke(func(server *app.Server) {
+		server.Init()
+	})
 	if err != nil {
 		return err
 	}
-	defer func() {
-		if cerr := db.Close(); cerr != nil {
-			if err == nil {
-				err = cerr
-				return
-			}
-			log.Println(err)
-		}
-	}()
 
-	mux := http.NewServeMux()
-	customerSvg := customers.NewService(db)
-	server := app.NewServer(mux, customerSvg)
-	server.Init()
+	return container.Invoke(func(server *http.Server) error {
+		return server.ListenAndServe()
+	})
 
-	srv := &http.Server{
-		Addr:    net.JoinHostPort(host, port),
-		Handler: server,
-	}
-	return srv.ListenAndServe()
+
 }
-
-//type Customer struct {
-//	ID      int64
-//	Name    string
-//	Phone   string
-//	Active  bool
-//	Created time.Time
-//}
-//
-//func main() {
-//	dsn := "postgres://app:pass@localhost:5432/db"
-//	db, err := sql.Open("pgx", dsn)
-//	if err != nil {
-//		log.Println(err)
-//		os.Exit(1)
-//		return
-//	}
-//	defer func() {
-//		if err := db.Close(); err != nil {
-//			log.Println(err)
-//		}
-//	}()
-//
-//	ctx := context.Background()
-//	_, err = db.ExecContext(ctx, `
-//CREATE TABLE IF NOT EXISTS  customers(
-//    id BIGSERIAL PRIMARY KEY,
-//    name TEXT NOT NULL,
-//    phone TEXT NOT NULL UNIQUE ,
-//    active BOOLEAN NOT NULL DEFAULT TRUE,
-//    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-//)`)
-//	if err != nil {
-//		log.Println(err)
-//		return
-//	}
-//	customer:=&Customer{}
-//	err=db.QueryRowContext(ctx,`
-//SELECT id, name, phone,active, created FROM customers WHERE id=1`).Scan(&customer.ID,&customer.Name,&customer.Phone, &customer.Active, &customer.Created)
-//	id:=1
-//	newPhone:="+992000000099"
-//	err=db.QueryRowContext(ctx,`
-//UPDATE customers SET phone=$2 WHERE id=$1 RETURNING id, name, phone, active, created`,
-//id, newPhone).Scan(&customer.ID, &customer.Name, &customer.Phone, &customer.Active, &customer.Created)
-//	if errors.Is(err, sql.ErrNoRows){
-//		log.Println("NO rows")
-//		return
-//	}
-//if err!=nil{
-//		log.Println(err)
-//		return
-//	}
-//	log.Printf("%#v", customer)
-//	log.Print(customer)
-
-//	name := "Petya"
-//	phone := "992000000001"
-//	result, err := db.ExecContext(ctx, `
-//INSERT INTO customers(name, phone) VALUES ($1, $2) ON CONFLICT (phone) DO UPDATE SET name =excluded.name;`, name, phone)
-//	if err != nil {
-//		log.Println(err)
-//		return
-//	}
-//	log.Println(result.RowsAffected())
-//	log.Println(result.LastInsertId())
-//}
-
-/*package main
-
-import (
-	"github.com/Shahlojon/crud/cmd/app"
-	"github.com/Shahlojon/crud/pkg/customers"
-	"net/http"
-	"database/sql"
-	_ "github.com/jackc/pgx/v4/stdlib"
-	"log"
-	"os"
-)
-
-func main() {
-	host :="0.0.0.0"
-	port := "9999"
-	dbConnectionString :="postgres://app:pass@localhost:5432/db"
-	if err := execute(host, port, dbConnectionString); err != nil{
-		log.Print(err)
-		os.Exit(1)
-	}
-}
-
-func execute(host, port, dbConnectionString string) (err error){
-	db, err := sql.Open("pgx", dbConnectionString)
-	if err !=nil{
-		return err
-	}
-	defer db.Close()
-
-	mux := http.NewServeMux()
-	customerService := customers.NewService(db)
-	server := app.NewServer(mux, customerService)
-	server.Init()
-
-	httpServer := &http.Server{
-		Addr:host+":"+port,
-		Handler: server,
-	}
-
-	return httpServer.ListenAndServe()
-}*/
--- pkg/customers/service.go
+++ pkg/customers/service.go
@@ -2,8 +2,9 @@ package customers
 
 import (
 	"context"
-	"database/sql"
 	"errors"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"log"
 	"time"
 )
@@ -16,172 +17,179 @@ var ErrInternal = errors.New("internal error")
 
 //Service ..
 type Service struct {
-db *sql.DB
+	//db *sql.DB
+	pool *pgxpool.Pool
 }
 
 //NewService ..
-func NewService(db *sql.DB) *Service {
-return &Service{db: db}
+func NewService(pool *pgxpool.Pool) *Service {
+	return &Service{pool: pool}
 }
 
 //Customer ...
 type Customer struct {
-ID      int64     `json:"id"`
-Name    string    `json:"name"`
-Phone   string    `json:"phone"`
-Active  bool      `json:"active"`
-Created time.Time `json:"created"`
+	ID      int64     `json:"id"`
+	Name    string    `json:"name"`
+	Phone   string    `json:"phone"`
+	Active  bool      `json:"active"`
+	Created time.Time `json:"created"`
 }
-
 
 func (s *Service) All(ctx context.Context) (cs []*Customer, err error) {
 
-sqlStatement := `select * from customers`
+	sqlStatement := `select * from customers`
 
-rows, err := s.db.QueryContext(ctx, sqlStatement)
-if err != nil {
-return nil, err
-}
-defer rows.Close()
+	rows, err := s.pool.Query(ctx, sqlStatement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-for rows.Next() {
-item := &Customer{}
-err := rows.Scan(
-&item.ID,
-&item.Name,
-&item.Phone,
-&item.Active,
-&item.Created,
-)
-if err != nil {
-log.Println(err)
-}
-cs = append(cs, item)
-}
+	for rows.Next() {
+		item := &Customer{}
+		err := rows.Scan(
+			&item.ID,
+			&item.Name,
+			&item.Phone,
+			&item.Active,
+			&item.Created,
+		)
+		if err != nil {
+			log.Println(err)
+		}
+		cs = append(cs, item)
+	}
 
-return cs, nil
+	return cs, nil
 }
 
 func (s *Service) AllActive(ctx context.Context) (cs []*Customer, err error) {
+	rows, err := s.pool.Query(ctx, `select * from customers where active=true`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-sqlStatement := `select * from customers where active=true`
+	for rows.Next() {
+		item := &Customer{}
+		err := rows.Scan(
+			&item.ID,
+			&item.Name,
+			&item.Phone,
+			&item.Active,
+			&item.Created,
+		)
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNotFound
+		}
 
-rows, err := s.db.QueryContext(ctx, sqlStatement)
-if err != nil {
-return nil, err
-}
-defer rows.Close()
+		if err != nil {
+			log.Println(err)
+			return nil, ErrInternal
+		}
+		cs = append(cs, item)
+	}
 
-for rows.Next() {
-item := &Customer{}
-err := rows.Scan(
-&item.ID,
-&item.Name,
-&item.Phone,
-&item.Active,
-&item.Created,
-)
-if err != nil {
-log.Println(err)
-}
-cs = append(cs, item)
-}
-
-return cs, nil
+	return cs, nil
 }
 
 func (s *Service) ByID(ctx context.Context, id int64) (*Customer, error) {
-item := &Customer{}
+	item := &Customer{}
 
-sqlStatement := `select * from customers where id=$1`
-err := s.db.QueryRowContext(ctx, sqlStatement, id).Scan(
-&item.ID,
-&item.Name,
-&item.Phone,
-&item.Active,
-&item.Created)
+	err := s.pool.QueryRow(ctx, `
+SELECT id, name, phone, active, created FROM customers WHERE id=$1`, id).Scan(
+		&item.ID,
+		&item.Name,
+		&item.Phone,
+		&item.Active,
+		&item.Created)
 
-if err == sql.ErrNoRows {
-return nil, ErrNotFound
-}
-if err != nil {
-log.Print(err)
-return nil, ErrInternal
-}
-return item, nil
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, ErrNotFound
+	}
+
+	if err != nil {
+		log.Println(err)
+		return nil, ErrInternal
+	}
+	return item, nil
 
 }
 
 func (s *Service) ChangeActive(ctx context.Context, id int64, active bool) (*Customer, error) {
-item := &Customer{}
+	item := &Customer{}
 
-sqlStatement := `update customers set active=$2 where id=$1 returning *`
-err := s.db.QueryRowContext(ctx, sqlStatement, id, active).Scan(
-&item.ID,
-&item.Name,
-&item.Phone,
-&item.Active,
-&item.Created)
+	err := s.pool.QueryRow(ctx, `
+UPDATE customers SET active=$2 WHERE id=$1 RETURNING *`, id, active).Scan(
+		&item.ID,
+		&item.Name,
+		&item.Phone,
+		&item.Active,
+		&item.Created)
 
-if err == sql.ErrNoRows {
-return nil, ErrNotFound
-}
-if err != nil {
-log.Print(err)
-return nil, ErrInternal
-}
-return item, nil
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, ErrNotFound
+	}
+
+	if err != nil {
+		log.Println(err)
+		return nil, ErrInternal
+	}
+	return item, nil
 
 }
 
 func (s *Service) Delete(ctx context.Context, id int64) (*Customer, error) {
-item := &Customer{}
+	item := &Customer{}
 
-sqlStatement := `delete from customers  where id=$1 returning *`
-err := s.db.QueryRowContext(ctx, sqlStatement, id).Scan(
-&item.ID,
-&item.Name,
-&item.Phone,
-&item.Active,
-&item.Created)
+	err := s.pool.QueryRow(ctx, `
+DELETE FROM customers  WHERE id=$1 RETURNING *`, id).Scan(
+		&item.ID,
+		&item.Name,
+		&item.Phone,
+		&item.Active,
+		&item.Created)
 
-if err == sql.ErrNoRows {
-return nil, ErrNotFound
-}
-if err != nil {
-log.Print(err)
-return nil, ErrInternal
-}
-return item, nil
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, ErrNotFound
+	}
+
+	if err != nil {
+		log.Println(err)
+		return nil, ErrInternal
+	}
+	return item, nil
 
 }
 
 func (s *Service) Save(ctx context.Context, customer *Customer) (c *Customer, err error) {
 
-item := &Customer{}
+	item := &Customer{}
 
-if customer.ID == 0 {
-sqlStatement := `insert into customers(name, phone) values($1, $2) returning *`
-err = s.db.QueryRowContext(ctx, sqlStatement, customer.Name, customer.Phone).Scan(
-&item.ID,
-&item.Name,
-&item.Phone,
-&item.Active,
-&item.Created)
-} else {
-sqlStatement := `update customers set name=$1, phone=$2 where id=$3 returning *`
-err = s.db.QueryRowContext(ctx, sqlStatement, customer.Name, customer.Phone, customer.ID).Scan(
-&item.ID,
-&item.Name,
-&item.Phone,
-&item.Active,
-&item.Created)
-}
+	if customer.ID == 0 {
+		err = s.pool.QueryRow(ctx, `INSERT INTO customers(name, phone) values($1, $2) RETURNING *`, customer.Name, customer.Phone).Scan(
+			&item.ID,
+			&item.Name,
+			&item.Phone,
+			&item.Active,
+			&item.Created)
+	} else {
+		err = s.pool.QueryRow(ctx, `UPDATE customers SET name=$1, phone=$2 where id=$3 RETURNING *`, customer.Name, customer.Phone, customer.ID).Scan(
+			&item.ID,
+			&item.Name,
+			&item.Phone,
+			&item.Active,
+			&item.Created)
+	}
 
-if err != nil {
-log.Print(err)
-return nil, ErrInternal
-}
-return item, nil
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, ErrNotFound
+	}
+
+	if err != nil {
+		log.Println(err)
+		return nil, ErrInternal
+	}
+	return item, nil
 
 }
